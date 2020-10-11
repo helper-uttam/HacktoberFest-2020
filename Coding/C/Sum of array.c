664f75127dd6fc5bb6eeddcd82efad4b66fd1aae
helper-uttam <72701081+helper-uttam@users.noreply.github.com>
2020-10-11 22:08:47 +0530

    Update Sum of array.c

--- Coding/C/Sum of array.c
+++ Coding/C/Sum of array.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-int main() {
-    int ct,num,sum=0;
+int main() 
+{
+    int ct,num,sum=0;//defining the variable 1st before using in array
     scanf("%d",&num);
-    int array[num];
+    int array[num]; //num=size of the array
     for(ct=0;ct<num;ct++)
     {
         scanf("%d ",&array[ct]);
         sum+=array[ct];
     }
     
-    printf("Sum of array is %d",sum);
+    printf("Sum of array is %d",sum);//final result
 }
